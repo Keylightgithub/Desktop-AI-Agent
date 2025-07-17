a76b7abad88304b3eec2f247bfd95f7cea405fca
Isaac Henry <129680840+Keylightgithub@users.noreply.github.com>
2025-07-17 02:22:16 -0500

    Update Desktop_Agentic_AI.py
    added more intro info

--- Desktop_Agentic_AI.py
+++ Desktop_Agentic_AI.py
@@ -7,17 +7,17 @@ from env import GOOGLE_API_KEY
 
 '''
 How to use:
-1. Only change the prompt Goal and Operating system in the prompt variable.
-2. Once the script is ran, the script will hide the first window,
+1. Add your Google API key to env.py file
+2. Only change the prompt Goal and Operating system in the prompt variable below.
+
+Description:
+1. Once the script is ran, the script will hide the first window,
 2. Next the second window will be the one that the screenshot will take and the AI will analyze.
 3. The AI will generate a list of steps to accomplish the goal.
 4. The script will parse the AI response and execute the steps using pyautogui.
 5. The script will filter out some letters from the results if conflicting with MacOS modifier keys.
 6. The script will execute the steps with a delay to allow for proper execution.
-'''
 
-
-'''
 Note:
 Some keys are filtered out since MacOS may mistake them for modifier keys.
 For example, 'm' and 'M' are filtered out from pyautogui commands
